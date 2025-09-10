c6274ad24846dbccaa407b3790c31e00deb2d8da
Pashatania Fitri Indah Lestari <87978506+pashatania1@users.noreply.github.com>
2025-09-10 14:22:51 +0700

    Add files via upload

--- Input.java
+++ Input.java
@@ -3,11 +3,14 @@ import java.util.Scanner;
 public class Input {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        System.out.println("Input Sikap: (A-D)");
+        String sikap = in.nextLine();
         System.out.println("Input Nilai:");
         int nilai = in.nextInt();
 
+        System.out.println("Sikap : " + sikap);
         System.out.println("Nilai : " + nilai);
-        if(nilai >= 75 ) {
+        if(nilai >= 75 && (sikap.equals("A") || sikap.equals("B"))) {
             System.out.println("Lulus");
         } else {
             System.out.println("Remidi");
@@ -17,6 +20,8 @@ public class Input {
             System.out.println("Sangat Baik");
         } else if(nilai >= 75) {
             System.out.println("Baik");
+        } else if(nilai >= 50) {
+            System.out.println("Cukup");
         } else {
             System.out.println("Remidi");
         }
